49f6b21baa1cca789a4a3ee31ccf32ff29982dd1
nikolaine <niko121879@gmail.com>
2023-02-17 14:05:17 -0800

    create reducer with slice

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import {BrowserRouter, Route, Routes} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Layout from './layouts';
 import Home from './pages/Home';
 import Blogs from './pages/Blogs';
 import Contact from './pages/Contact';
 import NoPage from './pages/NoPage';
+import { store } from './store';
+import { Provider } from 'react-redux';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Layout />}>
-          <Route index element={<Home />}/>
-          <Route path="blogs" element={<Blogs />}/>
-          <Route path="contact" element={<Contact />}/>
-          <Route path="*" element={<NoPage />}/>
-        </Route>
-      </Routes>
-    </BrowserRouter>
+    <Provider store={store}>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="blogs" element={<Blogs />} />
+            <Route path="contact" element={<Contact />} />
+            <Route path="*" element={<NoPage />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </Provider>
   );
 }
 
--- src/store/index.js
+++ src/store/index.js
@@ -1,35 +1,8 @@
-const initialState = {
-    visibilityFilter: 'SHOW_ALL',
-    todos: []
-}
+import { configureStore } from "@reduxjs/toolkit"
+import counterReducer from "./slices/counterSlice"
 
-function appReducer(state = initialState, action) {
-    switch(action.type) {
-        case 'SET_VISIBILITY_FILTER': {
-            return Object.assign({}, state, {
-                visibilityFilter: action.filter
-            });
-        }
-        case 'ADD_TODO': {
-            return Object.assign({}, state, {
-                todos: state.todos.concat({
-                    id: action.id,
-                    text: action.text,
-                    completed: false
-                })
-            });
-        }
-        case 'TOGGLE_TODO': {
-            return Object.assign({}, state, {
-                todos: state.todos.map(todo => {
-                    if (todo.id !== action.id) {
-                        return todo
-                    }
-                    return Object.assign({}, todo, {
-                        completed: !todo.completed
-                    })
-                })
-            })
-        }  
-    }
-}
+export const store = configureStore({
+    reducer: {
+        counter: counterReducer,
+    },
+})
